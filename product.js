7e472a2b7724b7f2b42352775b53f382b9ab779a
Jamesmalo <9jmalo@stpatrick.org>
2017-10-17 14:45:20 -0500

    day 1

---
+++ product.js
@@ -0,0 +1,23 @@
+const Product = function(){
+
+let name = n;
+let price = p;
+let space = s;
+let prime = pr;
+
+  function makeName(){
+    for(let x=0;x<10;x++){
+      let name ="";
+      let c = Math.floor(25*Math.random())+65;
+      let letter = String.fromCharCode(c);
+      name += letter;
+    }
+  }
+  function makePrice(){
+    for(let x=0;x<10;x++){
+      let price = Math.floor(Math.random)
+    }
+  }
+
+
+}
